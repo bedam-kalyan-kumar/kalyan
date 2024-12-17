786ba56da67336cffaba8896e6653081618c98d7
bedam kalyan kumar <149291008+bedam-kalyan-kumar@users.noreply.github.com>
2024-12-17 11:36:51 +0530

    Delete kalyan directory

--- kalyan/projectname/kalyanapp/models.py
+++
@@ -1,9 +0,0 @@
-# models.py
-from django.db import models
-
-class MyModel(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
